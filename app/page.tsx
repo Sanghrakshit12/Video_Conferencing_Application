6813bc34abae486c101738abd77c0f19a961c5f7
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-03-12 01:48:54 +0530

    env

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-
 export default function Home() {
   return (
     <>
+      <div className="flex flex-row">
+        <div className="basis-1/4 bg-red-500">01</div>
+        <div className="basis-1/4 bg-neutral-500">02</div>
+        <div className="basis-1/2 bg-stone-500">03</div>
+      </div>
       <h1>Welcome to the Project</h1>
     </>
   );
