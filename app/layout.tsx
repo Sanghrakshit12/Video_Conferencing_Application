6f50b317c26a59d40b577d9a5ef6d8b519d3905c
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-03-27 10:23:20 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Provider from "./lib/provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Video Conferrencing App",
+  title: "NexMeet",
   description: "Welcome to NexMeet, your all-in-one solution for immersive and seamless video conferencing. Built with cutting-edge NexJS technology, NexMeet redefines the virtual collaboration experience, making remote meetings as effective and engaging as in-person interactions",
 };
 
