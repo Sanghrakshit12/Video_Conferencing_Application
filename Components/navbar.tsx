439ab3a0021075fbf24d6c34152fe8011fa9ccf3
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-03-27 10:43:19 +0530

    icon

--- Components/navbar.tsx
+++ Components/navbar.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 import {Video} from 'lucide-react'
-import LogoutButton from "./forms/authbutton"
 import AuthButtons from "./forms/authbutton"
 
-export default function Navbar(){
+
+ 
+export default async function Navbar(){
     return <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
         <div className="container flex items-center justify-between">
             <Link href={"/"}><Video size={36} /></Link>
             <AuthButtons />
-            
         </div>
     </div>
 }
