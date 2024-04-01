818a220c1d82e6f859577cd2353b22b2b1c69d36
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-04-01 19:10:41 +0530

    unauthorized access to admin route denied

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,11 +1,4 @@
-import { Next_Auth_Config } from "@/app/lib/auth";
-import { getServerSession } from "next-auth";
 
 export default async function Admin() {
-  const session = await getServerSession(Next_Auth_Config);
-  if (session?.user) {
-    return <div className="text-3xl">Application Logic</div>;
-  } else {
-    return <div className="text-3xl">Sign in To Use the Application</div>;
-  }
+ 
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,24 @@
 import Navbar from "@/Components/navbar";
 import ClientProvider from "../lib/ClientProvider";
+import { Next_Auth_Config } from "../lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { toast } from "@/Components/ui/use-toast";
 
 export default async function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <ClientProvider>
-      <Navbar />
-      <div>{children}</div>
-    </ClientProvider>
-  );
+  const session = await getServerSession(Next_Auth_Config);
+  if (session?.user) {
+    return (
+      <ClientProvider>
+        <Navbar />
+        <div>{children}</div>
+      </ClientProvider>
+    );
+  } else {
+    redirect("/")
+  }
 }
