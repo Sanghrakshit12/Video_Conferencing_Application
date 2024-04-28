b7239a287480a00cf2985f245d59faa90075c9dc
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-04-29 00:01:45 +0530

    deploy3

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/user',
-        destination: 'http://localhost:3000/api/user*'
-      }
-    ]
   }
 };
 
