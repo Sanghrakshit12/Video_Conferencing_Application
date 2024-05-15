444e2e063d566da34c6a3aaf18f677a579b6de5e
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-05-15 19:23:11 +0530

    test

--- app/lib/ClientProvider.tsx
+++ app/lib/ClientProvider.tsx
@@ -13,14 +13,7 @@ interface ClientProviderProps {
   children: React.ReactNode;
 }
 
-export default function ClientProvider({ children }: ClientProviderProps) {
-  const videoClient = useInitializeVideoClient();
-  if (!videoClient) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <Loader2 className="mx-auto animate-spin" />
-      </div>
-    );
+export default function ClientProvider({ ch
   }
   return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 }
